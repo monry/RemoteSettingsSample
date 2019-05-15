b6d549eddb83ab7223ac99e0c02f5cd48e693a57
Tetsuya Mori <monry84@gmail.com>
2019-05-15 23:44:33 +0900

    :sparkles: Setup installer

--- Assets/Scripts/Application/Installer/SampleSceneInstaller.cs
+++ Assets/Scripts/Application/Installer/SampleSceneInstaller.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
 using RemoteSettingsSample.Application.ValueObject;
+using RemoteSettingsSample.Data.Repository.Implement;
+using RemoteSettingsSample.Domain.Entity.Implement;
+using RemoteSettingsSample.Domain.UseCase.Implement;
+using RemoteSettingsSample.Presentation.Presenter.Implement;
 using UnityEngine;
 using Zenject;
 
@@ -12,6 +16,21 @@ namespace RemoteSettingsSample.Application.Installer
 
         public override void InstallBindings()
         {
+            // UseCases
+            Container.BindInterfacesTo<ChangeSeasonText>().AsCached();
+            Container.BindInterfacesTo<HandleRemoteSetting>().AsCached();
+
+            // Entities
+            Container.BindInterfacesTo<SeasonTextMaster>().AsCached();
+            Container.BindInterfacesTo<SeasonState>().AsCached();
+
+            // Presenters
+            Container.BindInterfacesTo<SampleScene>().AsCached();
+
+            // Repositories
+            Container.BindInterfacesTo<SettingHandler>().AsCached();
+
+            // ValueObjects
             Container.BindInstance(SeasonTexts);
         }
     }
