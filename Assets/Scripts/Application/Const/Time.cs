77576ac3aa9c2b5513aee2e291f2cf869d3b48a4
Tetsuya Mori <monry84@gmail.com>
2019-05-17 00:38:01 +0900

    :sparkles: Define constant about time

---
+++ Assets/Scripts/Application/Const/Time.cs
@@ -0,0 +1,12 @@
+namespace RemoteSettingsSample.Application
+{
+    public static partial class Const
+    {
+        public static class Time
+        {
+            public const float IntervalRefresh = 5.0f;
+            public const double IntervalTextTypeWriter = 2.0;
+            public const double TweenDurationColorTransition = 2.0;
+        }
+    }
+}
